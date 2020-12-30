452e16c293d3d57a4d376176f237c3a3ce8c3b92
andrahafiz <andra18ti@mahasiswa.pcr.ac.id>
2020-12-30 18:46:28 +0700

    test

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -211,7 +211,7 @@ class Webhook extends Controller
     private function test($replyToken)
     {
         $data = $this->templateGateway->getData();
-        $sting = $data['judul_template'];
+        $sting = print_r($data['judul_template']);
 
         // if ($data != null) {
         //     foreach ($data as $user) {
