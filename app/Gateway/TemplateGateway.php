868d6647112fc838c18a215e2f72fa7b7800b497
andrahafiz <andra18ti@mahasiswa.pcr.ac.id>
2020-12-30 15:36:03 +0700

    deploy

---
+++ app/Gateway/TemplateGateway.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Gateway;
+
+use Illuminate\Database\ConnectionInterface;
+
+class TemplateGateway
+{
+    /**
+     * @var ConnectionInterface
+     */
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = app('db');
+    }
+
+    // Question
+    function getData()
+    {
+        $data = $this->db->table('template')
+            ->where('rating', 4);
+
+        if ($data) {
+            return (array) $data;
+        }
+
+        return null;
+    }
+
+
+
+    function isAnswerEqual(int $number, string $answer)
+    {
+        return $this->db->table('questions')
+            ->where('number', $number)
+            ->where('answer', $answer)
+            ->exists();
+    }
+}
---
+++ app/Http/Controllers/Webhook.php
@@ -0,0 +1,289 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Gateway\EventLogGateway;
+use App\Gateway\TemplateGateway;
+use App\Gateway\UserGateway;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Log\Logger;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\CarouselContainerBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\BubbleContainerBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\ImageComponentBuilder;
+use LINE\LINEBot\Constant\Flex\ComponentLayout;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\ButtonComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\IconComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder;
+use LINE\LINEBot\MessageBuilder\FlexMessageBuilder;
+
+class Webhook extends Controller
+{
+    /**
+     * @var LINEBot
+     */
+    private $bot;
+    /**
+     * @var Request
+     */
+    private $request;
+    /**
+     * @var Response
+     */
+    private $response;
+    /**
+     * @var Logger
+     */
+    private $logger;
+    /**
+     * @var EventLogGateway
+     */
+    private $logGateway;
+    /**
+     * @var UserGateway
+     */
+    private $userGateway;
+    /**
+     * @var TemplateGateway
+     */
+    private $templateGateway;
+    /**
+     * @var array
+     */
+    private $user;
+
+
+    public function __construct(
+        Request $request,
+        Response $response,
+        Logger $logger,
+        // EventLogGateway $logGateway,
+        UserGateway $userGateway,
+        TemplateGateway $templateGateway
+    ) {
+        $this->request = $request;
+        $this->response = $response;
+        $this->logger = $logger;
+        // $this->logGateway = $logGateway;
+        $this->userGateway = $userGateway;
+        $this->templateGateway = $templateGateway;
+        // $this->questionGateway = $questionGateway;
+
+        // create bot object
+        $httpClient = new CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+        $this->bot  = new LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+    }
+
+    public function __invoke()
+    {
+        // get request
+        $body = $this->request->all();
+
+        // debuging data
+        $this->logger->debug('Body', $body);
+
+        // save log
+        $signature = $this->request->server('HTTP_X_LINE_SIGNATURE') ?: '-';
+
+        return $this->handleEvents();
+    }
+
+    private function handleEvents()
+    {
+        $data = $this->request->all();
+
+        if (is_array($data['events'])) {
+            foreach ($data['events'] as $event) {
+                // skip group and room event
+                if (!isset($event['source']['userId'])) continue;
+
+                // get user data from database
+                $this->user = $this->userGateway->getUser($event['source']['userId']);
+
+                // if user not registered
+                if (!$this->user) $this->followCallback($event);
+                else {
+                    // respond event
+                    if ($event['type'] == 'message') {
+                        if (method_exists($this, $event['message']['type'] . 'Message')) {
+                            $this->{$event['message']['type'] . 'Message'}($event);
+                        }
+                    } else {
+                        if (method_exists($this, $event['type'] . 'Callback')) {
+                            $this->{$event['type'] . 'Callback'}($event);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        $this->response->setContent("No events found!");
+        $this->response->setStatusCode(200);
+        return $this->response;
+    }
+
+
+
+    private function followCallback($event)
+    {
+        $res = $this->bot->getProfile($event['source']['userId']);
+        if ($res->isSucceeded()) {
+            $profile = $res->getJSONDecodedBody();
+
+            // create welcome message
+            $message  = "Salam kenal, " . $profile['displayName'] . "!\n";
+            $message .= "Silakan kirim pesan \"MULAI\" untuk memulai kuis Tebak Kode.";
+            $textMessageBuilder = new TextMessageBuilder($message);
+
+            // create sticker message
+            $stickerMessageBuilder = new StickerMessageBuilder(1, 3);
+
+            // merge all message
+            $multiMessageBuilder = new MultiMessageBuilder();
+            $multiMessageBuilder->add($textMessageBuilder);
+            $multiMessageBuilder->add($stickerMessageBuilder);
+
+            // send reply message
+            $this->bot->replyMessage($event['replyToken'], $multiMessageBuilder);
+
+            // save user data
+            $this->userGateway->saveUser(
+                $profile['userId'],
+                $profile['displayName']
+            );
+        }
+    }
+
+    private function textMessage($event)
+    {
+        $userMessage = $event['message']['text'];
+        if (strtolower($userMessage) == 'template admin') {
+            $this->sendQuestion($event['replyToken']);
+        } else {
+            $message = 'Sepertinya kamu mengetikan perintah yang tidak ada.';
+            $textMessageBuilder = new TextMessageBuilder($message);
+
+            $message2 = "Berikut beberapa pilihan perintah yang tersedia " . "!\n" . "1. Template Admin " . "!\n" . "2. Template Lainnya";
+            $textMessageBuilder2 = new TextMessageBuilder($message2);
+
+            $stickerMessageBuilder = new StickerMessageBuilder(1, 10);
+            $multiMessageBuilder = new MultiMessageBuilder();
+            $multiMessageBuilder->add($textMessageBuilder);
+            $multiMessageBuilder->add($stickerMessageBuilder);
+            $multiMessageBuilder->add($textMessageBuilder2);
+
+            $this->bot->replyMessage($event['replyToken'], $multiMessageBuilder);
+        };
+    }
+    private function stickerMessage($event)
+    {
+        // create sticker message
+        $stickerMessageBuilder = new StickerMessageBuilder(1, 106);
+
+        // create text message
+        $message = 'Silakan kirim pesan "MULAI" untuk memulai kuis.';
+        $textMessageBuilder = new TextMessageBuilder($message);
+
+        // merge all message
+        $multiMessageBuilder = new MultiMessageBuilder();
+        $multiMessageBuilder->add($stickerMessageBuilder);
+        $multiMessageBuilder->add($textMessageBuilder);
+
+        // send message
+        $this->bot->replyMessage($event['replyToken'], $multiMessageBuilder);
+    }
+
+    private function sendQuestion($replyToken)
+    {
+        //   // get question from database
+        //   $question = $this->TemplateGateway->getData($questionNum);
+
+        //   // prepare answer options
+        //   for ($opsi = "a"; $opsi <= "d"; $opsi++) {
+
+
+        $data = $this->templateGateway->getData();
+
+        for ($i = 1; $i < 3; $i++) {
+            $options[] = $data['judul_template'];
+        }
+        $builder = new CarouselContainerBuilder([
+            BubbleContainerBuilder::builder()
+                ->setDirection("ltr")
+                ->setHero(
+                    // new ImageComponentBuilder('https://d17ivq9b7rppb3.cloudfront.net/original/commons/home-hero-new.jpg')
+                    ImageComponentBuilder::builder()
+                        ->setUrl('https://d17ivq9b7rppb3.cloudfront.net/original/commons/home-hero-new.jpg')
+                        ->setSize("full")
+                        ->setAspectRatio("320:213")
+                        ->setAspectMode("cover")
+                )->setBody(
+                    BoxComponentBuilder::builder()
+                        ->setLayout("vertical")
+                        ->setSpacing("sm")
+                        ->setPaddingAll("13px")
+                        ->setContents(
+                            [
+                                new TextComponentBuilder($options[1], null, null, "sm", null, null, true, null, 'bold'),
+                                new BoxComponentBuilder(
+                                    'baseline',
+                                    [
+                                        new IconComponentBuilder('https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png', null, "xs"),
+                                        new IconComponentBuilder('https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png', null, "xs"),
+                                        new IconComponentBuilder('https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png', null, "xs"),
+                                        new IconComponentBuilder('https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png', null, "xs"),
+                                        new IconComponentBuilder('https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png', null, "xs"),
+                                        new TextComponentBuilder('4.0', null, "md", "xs", null, null, null, null, null, "#8c8c8c")
+                                    ]
+                                ),
+                                new BoxComponentBuilder(
+                                    'vertical',
+                                    [
+                                        new BoxComponentBuilder(
+                                            'baseline',
+                                            [new TextComponentBuilder('Keterangan', 5, null, "xs", null, null, null, null, null, "#000000")],
+                                            null,
+                                            "sm"
+                                        )
+                                    ]
+                                )
+                            ]
+                        )
+
+                )->setFooter(
+                    BoxComponentBuilder::builder()
+                        ->setLayout("horizontal")
+                        ->setSpacing("sm")
+                        ->setContents(
+                            [
+                                new ButtonComponentBuilder(new UriTemplateActionBuilder('Priview', 'https://www.dicoding.com/'), null, null, null, "secondary"),
+                                new ButtonComponentBuilder(new UriTemplateActionBuilder('Download', 'https://www.dicoding.com/'), null, null, null, "primary")
+                            ]
+                        )
+                ),
+            BubbleContainerBuilder::builder()->setBody(
+                new BoxComponentBuilder(ComponentLayout::VERTICAL, [new TextComponentBuilder('World!')])
+            )
+        ]);
+
+        // build message
+        $messageBuilder = new FlexMessageBuilder("Gunakan mobile app untuk melihat soal", $builder);
+
+        // send message
+        $response = $this->bot->replyMessage($replyToken, $messageBuilder);
+    }
+}
