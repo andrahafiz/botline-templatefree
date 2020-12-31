37f546147c2b7e62bd2593f5f1df446d097978c0
andrahafiz <andra18ti@mahasiswa.pcr.ac.id>
2020-12-31 22:26:34 +0700

    test select

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -223,7 +223,9 @@ class Webhook extends Controller
         }
 
         foreach ($red as $value) {
-            $icon[] =  new IconComponentBuilder('https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png', null, "xs");
+            for ($i = 0; $i <= $value['rating']; $i++) {
+                $icon[] =  new IconComponentBuilder('https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png', null, "xs");
+            }
         }
         $icon[] = new TextComponentBuilder('4.0', null, "md", "xs", null, null, null, null, null, "#8c8c8c");
         $builder = new CarouselContainerBuilder([
