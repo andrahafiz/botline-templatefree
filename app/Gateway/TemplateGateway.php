2796657f4f56a6057037a882435a981a7059856e
andrahafiz <andra18ti@mahasiswa.pcr.ac.id>
2021-01-02 19:08:27 +0700

    commit

--- app/Gateway/TemplateGateway.php
+++ app/Gateway/TemplateGateway.php
@@ -19,8 +19,8 @@ class TemplateGateway
     // Question
     public function getData(string $type)
     {
-
-        $data = $this->db->select("select * from template  where tipe='" . preg_replace("Template", "", $type) . "' limit 5");
+        $keywoard = preg_replace("/Template/", "", $type);
+        $data = $this->db->select("select * from template  where tipe='" . $keywoard . "' limit 5");
         // $data = $this->db->table('template')->get()->first();
         if ($data) {
             return (array) $data;
